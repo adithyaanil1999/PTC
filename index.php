5ae3543793673b0c274b0ead11c31db77edf6179
adithya <adithyas.anil99@gmail.com>
2019-03-29 10:39:35 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/landing.css">
+    <link href="https://fonts.googleapis.com/css?family=Alike|Jaldi|Metrophobic" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <title>PTC</title>
+</head>
+<body>
+    <div class="main-container-landing">
+        <div class="container__welcome-msg ">
+            <div class="welcome-msg__Logo-container animated slideInUp faster"></div>
+            <div class="welcome-msg__about-Logo-container animated slideInUp">
+                    Placement and Training Cell Management Application               
+            </div>
+        </div>
+        <div class="container__signin">
+            <div class="signin__border-box ">
+                <div class="signin__box animated slideInUp">
+                    <div class="signin__box-header">
+                        <div class="signin__box-header__text">
+                            SIGN IN
+                        </div>
+                    </div>
+                    <form class="signin__box-form" method="POST" action="#">
+                        <div class="signin__box-form-input-wrapper">
+                            <div class="signin__box-input-header">Username:</div>                           
+                            <input class="signin__box-input-feild" name="username" type="text">
+                            <div class="signin__box-input-header">Password:</div>
+                            <input class="signin__box-input-feild" name="password" type="password">
+                        </div>
+                        <div class="signin__box-input-msg-wrapper"> 
+                            <span class="signin__box-input-msg animated shake fast">Incorrect password or username</span>
+                        </div>
+                    </form>
+                    <div class="signin__box-form-register-wrapper">
+                            Click here to <a href="">register</a> a new profile
+                    </div>
+                </div>
+            </div>
+            <div class="register__border-box">
+                <div class="register__box animated slideInUp">
+                    <div class="register__box-header">
+                        <div class="register__box-header__text">
+                            register
+                        </div>
+                    </div>
+                    <form class="register__box-form" method="POST" action="#">
+                        <div class="signin__box-form-input-wrapper">
+                            <div class="register__box-input-header">Username:</div>                           
+                            <input class="register__box-input-feild" name="username" type="text">
+                            <div class="register__box-input-header">Password:</div>
+                            <input class="register__box-input-feild" name="password" type="text">
+                        </div>
+                        <div class="register__box-input-msg-wrapper"> 
+                            <span class="register__box-input-msg animated shake fast">Incorrect password or username</span>
+                        </div>
+                    </form>
+                    <div class="register__box-form-register-wrapper">
+                            Click here to <a href="">register</a> a new profile
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex-break"></div>
+        <div class="container__footer">
+            Vellore Institute Of Technology,Vellore,632014
+        </div>
+    </div>
+</body>
+</html>
